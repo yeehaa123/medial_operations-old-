5f0d0ec3486d945527f56913fc3d16779c5c6f46
Yeehaa <medialoperations@gmail.com>
2012-09-25 17:09:45 +0200

    remove unwanted models

--- app/models/book_reference.rb
+++
@@ -1,5 +0,0 @@
-class MonographReference < CollectionReference
-  attr_accessible :chapter_references
-
-  has_many :chapters, class_name: 'ChapterReference', foreign_key: :type_id
-end
--- app/models/collection_reference.rb
+++
@@ -1,2 +0,0 @@
-class CollectionReference < Reference
-end
