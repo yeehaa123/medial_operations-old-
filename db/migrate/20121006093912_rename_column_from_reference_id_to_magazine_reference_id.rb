b71bcee1bd38cea74167287a2538aa6342d353ad
Yeehaa <medialoperations@gmail.com>
2012-10-06 23:30:39 +0200

    remove migration

--- db/migrate/20121006093912_rename_column_from_reference_id_to_magazine_reference_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnFromReferenceIdToMagazineReferenceId < ActiveRecord::Migration
-  def change
-    rename_column :magazine_publisheds, :reference_id, :magazine_article_reference_id
-  end
-end
